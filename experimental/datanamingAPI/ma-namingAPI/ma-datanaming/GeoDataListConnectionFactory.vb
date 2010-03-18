38863a02b44bc71fa43f2f86b7db68c5d935993e
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2010-03-18 00:59:46 +0000

    Two Factory classes added.

---
+++ experimental/datanamingAPI/ma-namingAPI/ma-datanaming/GeoDataListConnectionFactory.vb
@@ -0,0 +1,23 @@
+﻿
+Public Class GeoDataListConnectionFactory
+    Shared myFactory As GeoDataListConnectionFactory
+
+    Private Sub New()
+
+    End Sub
+
+    Public Shared Function getFactory() As GeoDataListConnectionFactory
+        If myFactory Is Nothing Then
+            myFactory = New GeoDataListConnectionFactory
+        End If
+
+        getFactory = myFactory
+    End Function
+
+    Public Function createGeoDataListConnection(ByVal listType As Integer, Optional ByRef args As String() = Nothing) As IGeoDataListConnection
+
+    End Function
+
+
+
+End Class
