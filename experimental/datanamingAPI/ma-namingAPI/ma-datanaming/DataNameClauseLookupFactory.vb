2c41931094144fa76b7d9c99966aa7969c800f0e
Andy Smith <asmith@mapaction.org>
2010-03-18 00:59:46 +0000

    Two Factory classes added.

---
+++ experimental/datanamingAPI/ma-namingAPI/ma-datanaming/DataNameClauseLookupFactory.vb
@@ -0,0 +1,20 @@
+﻿
+Public Class DataNameClauseLookupFactory
+    Shared myFactory As DataNameClauseLookupFactory
+
+    Private Sub New()
+    End Sub
+
+    Public Shared Function getFactory() As DataNameClauseLookupFactory
+        If myFactory Is Nothing Then
+            myFactory = New DataNameClauseLookupFactory
+        End If
+
+        getFactory = myFactory
+    End Function
+
+    Public Function createDataNameClauseLookup(ByVal dnclType As Integer, Optional ByRef args As String() = Nothing) As IDataNameClauseLookup
+
+    End Function
+
+End Class
--- experimental/datanamingAPI/ma-namingAPI/ma-datanaming/GeoDBDataNameClauseLookup.vb
+++ experimental/datanamingAPI/ma-namingAPI/ma-datanaming/GeoDBDataNameClauseLookup.vb
@@ -3,7 +3,7 @@ Public Class GeoDBDataNameClauseLookup
     Inherits AbstractDataNameClauseLookup
 
 
-
+    'ESRI.ArcGIS.Utility.Converter.ToDataSet
 
 
     
