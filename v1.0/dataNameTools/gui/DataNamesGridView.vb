f534d7a9369637378951cb1cfc03873f9502989c
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2010-11-23 15:43:47 +0000

    Removed any third-party sample code that might not have been compatable with the GPL v3

--- v1.0/dataNameTools/gui/DataNamesGridView.vb
+++ v1.0/dataNameTools/gui/DataNamesGridView.vb
@@ -50,12 +50,12 @@ Public Class DataNamesGridView
         'Dim column0 As New DataGridViewCheckBoxColumn()
         'column0.Name = "CheckBoxes"
 
-        Dim column1 As New DataGridViewDisableButtonColumn()
-        column1.Name = "Rename"
-        column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells
-        column1.DefaultCellStyle.BackColor = SystemColors.ButtonFace
+        Dim colRenameBtns As New DataNamesGridRenameButtonColumn()
+        colRenameBtns.Name = "Rename"
+        colRenameBtns.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells
+        colRenameBtns.DefaultCellStyle.BackColor = SystemColors.ButtonFace
 
-        datGV.Columns.Insert(m_intIdxButtn, column1)
+        datGV.Columns.Insert(m_intIdxButtn, colRenameBtns)
 
         For i = 0 To (datGV.Columns.Count - 1)
             System.Console.WriteLine(datGV.Columns.Item(i).Name & " : " & datGV.Columns.Item(i).Index)
@@ -135,8 +135,8 @@ Public Class DataNamesGridView
         End If
 
         'Add Button column
-        Dim buttonCell As New DataGridViewDisableButtonCell
-        buttonCell.Enabled = dName.isRenameable()
+        Dim buttonCell As New DataNamesGridRenameButtonCell
+        buttonCell.allowRename = dName.isRenameable()
         buttonCell.Value = "Rename..."
 
         rDGVRow.Cells.Item(m_intIdxButtn) = buttonCell
@@ -203,9 +203,9 @@ Public Class DataNamesGridView
 
         If ((e.ColumnIndex = m_intIdxButtn) And (e.RowIndex >= 0)) Then
 
-            Dim buttonCell As DataGridViewDisableButtonCell = _
-                CType(datGV.Rows(e.RowIndex).Cells(m_intIdxButtn), DataGridViewDisableButtonCell)
-            If buttonCell.Enabled Then
+            Dim buttonCell As DataNamesGridRenameButtonCell = _
+                CType(datGV.Rows(e.RowIndex).Cells(m_intIdxButtn), DataNamesGridRenameButtonCell)
+            If buttonCell.allowRename Then
                 'datGV.Rows(e.RowIndex).Cells(e.ColumnIndex).Value.ToString() & " is enabled: " & buttonCell.Enabled)
                 'm_dnRenameDialog.dataName = m_lstDataNames.Item(e.RowIndex)
                 m_dnRenameDialog.dataName = m_dicDataNames.Item(datGV.Rows.Item(e.RowIndex))
@@ -217,99 +217,3 @@ Public Class DataNamesGridView
 
 
 End Class
-
-Public Class DataGridViewDisableButtonColumn
-    Inherits DataGridViewButtonColumn
-
-    Public Sub New()
-        Me.CellTemplate = New DataGridViewDisableButtonCell()
-    End Sub
-End Class
-
-Public Class DataGridViewDisableButtonCell
-    Inherits DataGridViewButtonCell
-
-    Private enabledValue As Boolean
-    Public Property Enabled() As Boolean
-        Get
-            Return enabledValue
-        End Get
-        Set(ByVal value As Boolean)
-            enabledValue = value
-        End Set
-    End Property
-
-    ' Override the Clone method so that the Enabled property is copied.
-    Public Overrides Function Clone() As Object
-        Dim Cell As DataGridViewDisableButtonCell = _
-            CType(MyBase.Clone(), DataGridViewDisableButtonCell)
-        Cell.Enabled = Me.Enabled
-        Return Cell
-    End Function
-
-    ' By default, enable the button cell.
-    Public Sub New()
-        Me.enabledValue = True
-    End Sub
-
-    Protected Overrides Sub Paint(ByVal graphics As Graphics, _
-        ByVal clipBounds As Rectangle, ByVal cellBounds As Rectangle, _
-        ByVal rowIndex As Integer, _
-        ByVal elementState As DataGridViewElementStates, _
-        ByVal value As Object, ByVal formattedValue As Object, _
-        ByVal errorText As String, _
-        ByVal cellStyle As DataGridViewCellStyle, _
-        ByVal advancedBorderStyle As DataGridViewAdvancedBorderStyle, _
-        ByVal paintParts As DataGridViewPaintParts)
-
-        ' The button cell is disabled, so paint the border,  
-        ' background, and disabled button for the cell.
-        If Not Me.enabledValue Then
-
-            ' Draw the background of the cell, if specified.
-            If (paintParts And DataGridViewPaintParts.Background) = _
-                DataGridViewPaintParts.Background Then
-
-                Dim cellBackground As New SolidBrush(cellStyle.BackColor)
-                graphics.FillRectangle(cellBackground, cellBounds)
-                cellBackground.Dispose()
-            End If
-
-            ' Draw the cell borders, if specified.
-            If (paintParts And DataGridViewPaintParts.Border) = _
-                DataGridViewPaintParts.Border Then
-
-                PaintBorder(graphics, clipBounds, cellBounds, cellStyle, _
-                    advancedBorderStyle)
-            End If
-
-            ' Calculate the area in which to draw the button.
-            Dim buttonArea As Rectangle = cellBounds
-            Dim buttonAdjustment As Rectangle = _
-                Me.BorderWidths(advancedBorderStyle)
-            buttonArea.X += buttonAdjustment.X
-            buttonArea.Y += buttonAdjustment.Y
-            buttonArea.Height -= buttonAdjustment.Height
-            buttonArea.Width -= buttonAdjustment.Width
-
-            ' Draw the disabled button.                
-            ButtonRenderer.DrawButton(graphics, buttonArea, _
-                PushButtonState.Disabled)
-
-            ' Draw the disabled button text. 
-            If TypeOf Me.FormattedValue Is String Then
-                TextRenderer.DrawText(graphics, CStr(Me.FormattedValue), _
-                    Me.DataGridView.Font, buttonArea, SystemColors.GrayText)
-            End If
-
-        Else
-            ' The button cell is enabled, so let the base class 
-            ' handle the painting.
-            MyBase.Paint(graphics, clipBounds, cellBounds, rowIndex, _
-                elementState, value, formattedValue, errorText, _
-                cellStyle, advancedBorderStyle, paintParts)
-        End If
-    End Sub
-
-End Class
-
