1ae68181abcd5c39af74edf559464d7e4b4aaec7
gvaughan <gv-@users.noreply.github.com>
2012-12-21 14:15:32 +0000

    Prototype 1 first commit

---
+++ arcgis10_mapping_tools/Prototype1_ConfigTool/Prototype1_ConfigTool/frmCreateXml.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace Prototype1_ConfigTool
+{
+    partial class frmCreateXml
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCreateXml));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.tbxGlideNo = new System.Windows.Forms.TextBox();
+            this.lblGlideNo = new System.Windows.Forms.Label();
+            this.tbxOperationID = new System.Windows.Forms.TextBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.tbxOperationName = new System.Windows.Forms.TextBox();
+            this.label22 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCreateNewXmlPath = new System.Windows.Forms.Button();
+            this.tbxNewFileFolder = new System.Windows.Forms.TextBox();
+            this.lblCreateNewXml = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(8, 72);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(356, 340);
+            this.tabControl1.TabIndex = 14;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox13);
+            this.tabPage2.Controls.Add(this.label13);
+            this.tabPage2.Controls.Add(this.textBox14);
+            this.tabPage2.Controls.Add(this.label14);
+            this.tabPage2.Controls.Add(this.textBox15);
+            this.tabPage2.Controls.Add(this.label15);
+            this.tabPage2.Controls.Add(this.textBox16);
+            this.tabPage2.Controls.Add(this.label16);
+            this.tabPage2.Controls.Add(this.textBox17);
+            this.tabPage2.Controls.Add(this.label17);
+            this.tabPage2.Controls.Add(this.textBox18);
+            this.tabPage2.Controls.Add(this.label18);
+            this.tabPage2.Controls.Add(this.tbxGlideNo);
+            this.tabPage2.Controls.Add(this.lblGlideNo);
+            this.tabPage2.Controls.Add(this.tbxOperationID);
+            this.tabPage2.Controls.Add(this.label20);
+            this.tabPage2.Controls.Add(this.textBox21);
+            this.tabPage2.Controls.Add(this.label21);
+            this.tabPage2.Controls.Add(this.tbxOperationName);
+            this.tabPage2.Controls.Add(this.label22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(348, 314);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Section A";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Enabled = false;
+            this.textBox13.Location = new System.Drawing.Point(126, 278);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(200, 20);
+            this.textBox13.TabIndex = 41;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 281);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(50, 13);
+            this.label13.TabIndex = 40;
+            this.label13.Text = "Element j";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Enabled = false;
+            this.textBox14.Location = new System.Drawing.Point(126, 249);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(200, 20);
+            this.textBox14.TabIndex = 39;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(12, 252);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(50, 13);
+            this.label14.TabIndex = 38;
+            this.label14.Text = "Element i";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Enabled = false;
+            this.textBox15.Location = new System.Drawing.Point(126, 191);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(200, 20);
+            this.textBox15.TabIndex = 37;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(12, 194);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(54, 13);
+            this.label15.TabIndex = 36;
+            this.label15.Text = "Element g";
+            // 
+            // textBox16
+            // 
+            this.textBox16.Enabled = false;
+            this.textBox16.Location = new System.Drawing.Point(126, 162);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(200, 20);
+            this.textBox16.TabIndex = 35;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(12, 165);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(51, 13);
+            this.label16.TabIndex = 34;
+            this.label16.Text = "Element f";
+            // 
+            // textBox17
+            // 
+            this.textBox17.Enabled = false;
+            this.textBox17.Location = new System.Drawing.Point(126, 220);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(200, 20);
+            this.textBox17.TabIndex = 33;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(12, 223);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(54, 13);
+            this.label17.TabIndex = 32;
+            this.label17.Text = "Element h";
+            // 
+            // textBox18
+            // 
+            this.textBox18.Enabled = false;
+            this.textBox18.Location = new System.Drawing.Point(126, 133);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(200, 20);
+            this.textBox18.TabIndex = 31;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(12, 136);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(54, 13);
+            this.label18.TabIndex = 30;
+            this.label18.Text = "Element e";
+            // 
+            // tbxGlideNo
+            // 
+            this.tbxGlideNo.Location = new System.Drawing.Point(126, 75);
+            this.tbxGlideNo.Name = "tbxGlideNo";
+            this.tbxGlideNo.Size = new System.Drawing.Size(200, 20);
+            this.tbxGlideNo.TabIndex = 29;
+            // 
+            // lblGlideNo
+            // 
+            this.lblGlideNo.AutoSize = true;
+            this.lblGlideNo.Location = new System.Drawing.Point(12, 78);
+            this.lblGlideNo.Name = "lblGlideNo";
+            this.lblGlideNo.Size = new System.Drawing.Size(46, 13);
+            this.lblGlideNo.TabIndex = 28;
+            this.lblGlideNo.Text = "Glide no";
+            // 
+            // tbxOperationID
+            // 
+            this.tbxOperationID.Location = new System.Drawing.Point(126, 46);
+            this.tbxOperationID.Name = "tbxOperationID";
+            this.tbxOperationID.Size = new System.Drawing.Size(200, 20);
+            this.tbxOperationID.TabIndex = 27;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(12, 49);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(67, 13);
+            this.label20.TabIndex = 26;
+            this.label20.Text = "Operation ID";
+            // 
+            // textBox21
+            // 
+            this.textBox21.Enabled = false;
+            this.textBox21.Location = new System.Drawing.Point(126, 104);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(200, 20);
+            this.textBox21.TabIndex = 25;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(12, 107);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(54, 13);
+            this.label21.TabIndex = 24;
+            this.label21.Text = "Element d";
+            // 
+            // tbxOperationName
+            // 
+            this.tbxOperationName.Location = new System.Drawing.Point(126, 17);
+            this.tbxOperationName.Name = "tbxOperationName";
+            this.tbxOperationName.Size = new System.Drawing.Size(200, 20);
+            this.tbxOperationName.TabIndex = 23;
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(12, 20);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(82, 13);
+            this.label22.TabIndex = 22;
+            this.label22.Text = "Operation name";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(348, 314);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Section B";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(195, 420);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(276, 420);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 11;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCreateNewXmlPath);
+            this.groupBox1.Controls.Add(this.tbxNewFileFolder);
+            this.groupBox1.Controls.Add(this.lblCreateNewXml);
+            this.groupBox1.Location = new System.Drawing.Point(8, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(352, 52);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuration file path";
+            // 
+            // btnCreateNewXmlPath
+            // 
+            this.btnCreateNewXmlPath.Location = new System.Drawing.Point(312, 16);
+            this.btnCreateNewXmlPath.Name = "btnCreateNewXmlPath";
+            this.btnCreateNewXmlPath.Size = new System.Drawing.Size(33, 23);
+            this.btnCreateNewXmlPath.TabIndex = 1;
+            this.btnCreateNewXmlPath.Text = "...";
+            this.btnCreateNewXmlPath.UseVisualStyleBackColor = true;
+            this.btnCreateNewXmlPath.Click += new System.EventHandler(this.btnCreateNewXmlPath_Click);
+            // 
+            // tbxNewFileFolder
+            // 
+            this.tbxNewFileFolder.Location = new System.Drawing.Point(128, 16);
+            this.tbxNewFileFolder.Name = "tbxNewFileFolder";
+            this.tbxNewFileFolder.Size = new System.Drawing.Size(180, 20);
+            this.tbxNewFileFolder.TabIndex = 0;
+            // 
+            // lblCreateNewXml
+            // 
+            this.lblCreateNewXml.AutoSize = true;
+            this.lblCreateNewXml.Location = new System.Drawing.Point(6, 22);
+            this.lblCreateNewXml.Name = "lblCreateNewXml";
+            this.lblCreateNewXml.Size = new System.Drawing.Size(66, 13);
+            this.lblCreateNewXml.TabIndex = 8;
+            this.lblCreateNewXml.Text = "Select folder";
+            // 
+            // frmCreateXml
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 452);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCreateXml";
+            this.Text = "Create xml";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.TextBox tbxGlideNo;
+        private System.Windows.Forms.Label lblGlideNo;
+        private System.Windows.Forms.TextBox tbxOperationID;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.TextBox tbxOperationName;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCreateNewXmlPath;
+        private System.Windows.Forms.TextBox tbxNewFileFolder;
+        private System.Windows.Forms.Label lblCreateNewXml;
+    }
+}
