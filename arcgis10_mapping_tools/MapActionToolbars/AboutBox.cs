e63f8c188131a686bd704c9ff208e0acf56b18a8
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2014-08-17 11:55:00 +0100

    - Added additional tool to MapActionToolbars Addin which displays a basic "About" dialog box. - Set the minor version number of the MapActionToolbars Addin to auto-increment
    This should resolve Issue 88:
    https://code.google.com/p/mapaction-toolbox/issues/detail?id=88

---
+++ arcgis10_mapping_tools/MapActionToolbars/AboutBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace MapActionToolbars
+{
+    public class ma_addin_about_box : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        private string[] m_known_toolnames = {"MapAction_Alpha_ConfigTool_ConfigTool",
+                                                "Alpha_LayoutTool_LayoutTool",
+                                                "Alpha_ExportTool_ExportTool" };
+
+        private string m_thisaddin_desc;
+
+        public ma_addin_about_box()
+        {
+            // m_thisaddin_desc = "version " + MapActionToolbars.ThisAddIn.Date.ToString();
+            m_thisaddin_desc = "Version " + System.Reflection.Assembly.GetExecutingAssembly()
+                                           .GetName().Version.ToString()
+                                           + "\n" +
+                               "Build date " + MapActionToolbars.ThisAddIn.Date.ToString();
+        }
+
+        protected override void OnClick()
+        {
+            showDialog();
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        private void showDialog()
+        {
+            MessageBox.Show(m_thisaddin_desc, "About MapAction toolbox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+    }
+}
