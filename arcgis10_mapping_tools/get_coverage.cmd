ac402f30b17713a31e0446aeed9921c92db27bf8
Andy Smith <asmith@mapaction.org>
2017-02-22 14:48:57 +0000

    Revert "tweak script for running tests"
    This reverts commit db2409a1ea5468414099beac06946a562ddb267b.

--- arcgis10_mapping_tools/get_coverage.cmd
+++ arcgis10_mapping_tools/get_coverage.cmd
@@ -1,4 +1,4 @@
 
-%~dp0packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -returntargetcode "-filter:+[MapAction]* +[MapActionToolbars]* -[*Test]*" "-target:%~dp0run-unittests.cmd" "-output:%~dp0opencover-results.xml"
+%~dp0packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -returntargetcode "-filter:+[MapAction]* -[*Test]*" "-target:%~dp0run-unittests.cmd" "-output:%~dp0opencover-results.xml"
 
 %~dp0packages\ReportGenerator.2.4.4.0\tools\ReportGenerator.exe "-reports:%~dp0opencover-results.xml" "-targetdir:%~dp0coverage"
