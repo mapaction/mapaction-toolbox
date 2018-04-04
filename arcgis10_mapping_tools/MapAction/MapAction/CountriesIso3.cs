66bef4233c42f5c4ab5c0c2f417f0d8d28b75762
shurst-ma <shurst@mapaction.org>
2018-04-04 19:29:55 +0100

    Use OperationConfig object

---
+++ arcgis10_mapping_tools/MapAction/MapAction/CountriesIso3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MapAction
+{
+    [Serializable]
+    public class CountriesIso3
+    {
+        [XmlElement("country-iso3")]
+        public List<string> CountryIso3 { get; set; }
+    }
+}
---
+++ arcgis10_mapping_tools/MapAction/MapAction/DataFrameProperties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapAction
+{
+    public class DataFrameProperties
+    {
+        public string scale     { get; set;}
+        public string pageSize { get; set; }
+        public double xMin { get; set; }
+        public double yMin { get; set; }
+        public double xMax { get; set; }
+        public double yMax { get; set; }
+ 
+        public DataFrameProperties()
+        {
+            this.scale = null;
+            this.pageSize = null;
+            this.xMin = 0;
+            this.yMin = 0;
+            this.xMax = 0;
+            this.yMax = 0;
+        }
+
+        public DataFrameProperties(string scale, string pageSize, double xMin, double yMin, double xMax, double yMax)
+        {
+            this.scale = scale;
+            this.pageSize = pageSize;
+            this.xMin = xMin;
+            this.yMin = yMin;
+            this.xMax = xMax;
+            this.yMax = yMax;
+        }
+    }
+}
