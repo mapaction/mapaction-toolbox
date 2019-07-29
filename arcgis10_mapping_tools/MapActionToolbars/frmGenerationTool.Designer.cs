4d314a938b5efefaed5d4c96e06bd69a53f38c68
shurst-ma <shurst@mapaction.org>
2019-07-29 19:55:07 +0100

    Add icon to Map Automation Tool dialog box

--- arcgis10_mapping_tools/MapActionToolbars/frmGenerationTool.Designer.cs
+++ arcgis10_mapping_tools/MapActionToolbars/frmGenerationTool.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGenerationTool));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lblProductClassification = new System.Windows.Forms.Label();
             this.cboClassification = new System.Windows.Forms.ComboBox();
@@ -52,7 +53,7 @@
             this.groupBox1.Controls.Add(this.cboProductType);
             this.groupBox1.Location = new System.Drawing.Point(18, 19);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(562, 147);
+            this.groupBox1.Size = new System.Drawing.Size(650, 147);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Generate";
@@ -73,7 +74,7 @@
             this.cboClassification.FormattingEnabled = true;
             this.cboClassification.Location = new System.Drawing.Point(125, 65);
             this.cboClassification.Name = "cboClassification";
-            this.cboClassification.Size = new System.Drawing.Size(405, 21);
+            this.cboClassification.Size = new System.Drawing.Size(482, 21);
             this.cboClassification.TabIndex = 6;
             this.cboClassification.SelectedIndexChanged += new System.EventHandler(this.cbxClassification_SelectedIndexChanged);
             // 
@@ -119,13 +120,13 @@
             this.cboProductType.FormattingEnabled = true;
             this.cboProductType.Location = new System.Drawing.Point(125, 101);
             this.cboProductType.Name = "cboProductType";
-            this.cboProductType.Size = new System.Drawing.Size(405, 21);
+            this.cboProductType.Size = new System.Drawing.Size(482, 21);
             this.cboProductType.TabIndex = 0;
             this.cboProductType.SelectedIndexChanged += new System.EventHandler(this.cbxProductType_SelectedIndexChanged);
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(167, 175);
+            this.btnGenerate.Location = new System.Drawing.Point(229, 175);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(75, 23);
             this.btnGenerate.TabIndex = 1;
@@ -135,7 +136,7 @@
             // 
             // Cancel
             // 
-            this.Cancel.Location = new System.Drawing.Point(345, 175);
+            this.Cancel.Location = new System.Drawing.Point(379, 175);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(75, 23);
             this.Cancel.TabIndex = 2;
@@ -147,10 +148,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 220);
+            this.ClientSize = new System.Drawing.Size(691, 220);
             this.Controls.Add(this.Cancel);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmGenerationTool";
             this.Text = "Map Automation Tool";
             this.Load += new System.EventHandler(this.frmGenerationTool_Load);
