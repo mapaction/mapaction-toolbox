27296ab2be382dab7eab437767a81b5623a8306f
shurst-ma <shurst@mapaction.org>
2019-11-21 21:36:24 +0000

    Restore toolbox for Map Automation

--- arcgis10_mapping_tools/MapAction/MapAction/mapbook_export_tool/generate_product.py
+++ arcgis10_mapping_tools/MapAction/MapAction/mapbook_export_tool/generate_product.py
@@ -8,38 +8,26 @@ from os import listdir
 from os.path import isfile, join
 from arcpy import env
 import re
-import pycountry
 
-from mapactionpy_controller.crash_move_folder import CrashMoveFolder
-from mapactionpy_controller.event import Event
 from mapactionpy_arcmap import MapChef
 
 # Parameters:
 #  0. Product name
-#  1. Crash Move Folder
-#  2. Return - String result
+#  1. Country name
+#  2. Cookboook Json File
+#  3. Layer Properties Json File
+#  4. Crash Move Folder
+#  5. Layer directory
+#  6. Return - String result
 
 if __name__ == '__main__':
     map_doc = arcpy.mapping.MapDocument("current")
     productName = arcpy.GetParameterAsText(0)
-    crashMoveFolder = arcpy.GetParameterAsText(1)
-	
-    eventFilePath = os.path.join(crashMoveFolder, "event_description.json")
-    event = Event(eventFilePath)	
-    countryName = None
-    cmf = None
-
-    if event is not None:
-		cmf = CrashMoveFolder(os.path.join(event.cmf_descriptor_path, "cmf_description.json"))  
-		country = pycountry.countries.get(alpha_3=event.affected_country_iso3.upper())
-		countryName = country.name
-    else:
-        raise Exception("Error: Could not derive country from " + eventFilePath)
-
-    cookbookJsonFile = cmf.map_definitions		
-    layerPropertiesJsonFile = cmf.layer_properties	
-    layerDirectory = cmf.layer_rendering
-			
+    countryName = arcpy.GetParameterAsText(1)
+    cookbookJsonFile = arcpy.GetParameterAsText(2)
+    layerPropertiesJsonFile = arcpy.GetParameterAsText(3)
+    crashMoveFolder = arcpy.GetParameterAsText(4)
+    layerDirectory = arcpy.GetParameterAsText(5)
     chef = MapChef(map_doc, cookbookJsonFile, layerPropertiesJsonFile, crashMoveFolder, layerDirectory)
     chef.cook(productName, countryName)
-    arcpy.SetParameter(2, chef.report())
+    arcpy.SetParameter(6, chef.report())
