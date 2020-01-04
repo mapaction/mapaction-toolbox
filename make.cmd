28f064441e9ca21571fac5ea3ee44c696f164b50
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-01-04 00:38:26 +0000

    updated path to msbuild

--- make.cmd
+++ make.cmd
@@ -1,5 +1,5 @@
 echo Building...
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0arcgis10_mapping_tools/MapAction-toolbox.sln /t:build /p:PlatformTarget=x86 /p:Configuration=Release /maxcpucount
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %~dp0arcgis10_mapping_tools/MapAction-toolbox.sln /t:build /p:PlatformTarget=x86 /p:Configuration=Release /maxcpucount
 
 :: echo Copying...
 :: call %~dp0arcgis10_mapping_tools\arcaddins_for_testing\post_build_copy_addins.cmd
