283c74f041661ae562b30e2e76aff10740101191
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-01-16 18:46:12 +0000

    Update make.cmd

--- make.cmd
+++ make.cmd
@@ -1,12 +1,11 @@
 echo Getting dependancies
-dotnet add package Newtonsoft.Json --version 12.0.3
+powershell Install-Package Newtonsoft.Json -Force
 
 echo Building...
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" %~dp0arcgis10_mapping_tools/MapAction-toolbox.sln /t:build /p:PlatformTarget=x86 /p:Configuration=Release /maxcpucount
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" %~dp0arcgis10_mapping_tools/MapAction-toolbox.sln /t:build /p:PlatformTarget=x86 /p:Configuration=Release /maxcpucount
 
 :: echo Copying...
 :: call %~dp0arcgis10_mapping_tools\arcaddins_for_testing\post_build_copy_addins.cmd
 
 :: echo Running Tests...
 :: call %~dp0arcgis10_mapping_tools\get_coverage.cmd
-
