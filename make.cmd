86acdfb980014c457714bfdadfca4c650595d017
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-01-18 18:15:40 +0000

    add nuget and update path to msbuild.exe

--- make.cmd
+++ make.cmd
@@ -1,8 +1,9 @@
 echo Getting dependancies
-powershell Install-Package Newtonsoft.Json -Force
+C:\nuget\nuget.exe install %~dp0arcgis10_mapping_tools\.nuget\packages.config"
+C:\nuget\nuget.exe install %~dp0arcgis10_mapping_tools\MapAction\MapAction\packages.config"
 
 echo Building...
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" %~dp0arcgis10_mapping_tools/MapAction-toolbox.sln /t:build /p:PlatformTarget=x86 /p:Configuration=Release /maxcpucount
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %~dp0arcgis10_mapping_tools/MapAction-toolbox.sln /t:build /p:PlatformTarget=x86 /p:Configuration=Release /maxcpucount
 
 :: echo Copying...
 :: call %~dp0arcgis10_mapping_tools\arcaddins_for_testing\post_build_copy_addins.cmd
