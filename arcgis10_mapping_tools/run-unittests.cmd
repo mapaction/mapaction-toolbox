103c147c7d97fa74e0f8ffd6e0f2a15debaa05b5
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-01-21 22:50:30 +0000

    Use msi installed nunit

--- arcgis10_mapping_tools/run-unittests.cmd
+++ arcgis10_mapping_tools/run-unittests.cmd
@@ -2,4 +2,5 @@ echo off
 echo %date% - %time%
 mkdir "%~dp0unittestresults"
 echo on
-"%~dp0packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe" "%~dp0CommonTests\ExportIntergrationTests.nunit" --result "%~dp0unittestresults\TestResult.xml"
+::"%~dp0packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe"
+"C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console-x86.exe" "%~dp0CommonTests\ExportIntergrationTests.nunit" --result "%~dp0unittestresults\TestResult.xml"
