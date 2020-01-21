a216b7d6f3e6544ed311cd7c555a99209d716999
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-01-21 16:42:47 +0000

    fix nunit cmf line switch

--- arcgis10_mapping_tools/run-unittests.cmd
+++ arcgis10_mapping_tools/run-unittests.cmd
@@ -2,4 +2,4 @@ echo off
 echo %date% - %time%
 mkdir "%~dp0unittestresults"
 echo on
-"%~dp0packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe" "%~dp0CommonTests\ExportIntergrationTests.nunit" --output "%~dp0unittestresults\TestResult.xml"
+"%~dp0packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe" "%~dp0CommonTests\ExportIntergrationTests.nunit" --result "%~dp0unittestresults\TestResult.xml"
