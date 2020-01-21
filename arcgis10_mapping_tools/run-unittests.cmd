f6687f429e93d202c6b08aa044dcbbb21c01645a
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-01-21 16:12:23 +0000

    specific dir for unittest output

--- arcgis10_mapping_tools/run-unittests.cmd
+++ arcgis10_mapping_tools/run-unittests.cmd
@@ -1,4 +1,5 @@
 echo off
 echo %date% - %time%
+mkdir "%~dp0unittestresults"
 echo on
-"%~dp0packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe" "%~dp0CommonTests\ExportIntergrationTests.nunit"
+"%~dp0packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe" "%~dp0CommonTests\ExportIntergrationTests.nunit" --output "%~dp0unittestresults\TestResult.xml"
